d83b3abb69ab64c7fd0a50041ae2ddebbdf66207
Pavel <76658162+Pavel253@users.noreply.github.com>
2024-03-02 03:22:19 +0300

    Разработка баз данных

--- app.py
+++ app.py
@@ -1,17 +1,26 @@
-from flask import Flask, render_template, request, redirect
+import os
+from flask import Flask, render_template, request, redirect, send_file, jsonify, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail, Message
+from db import *
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///newflask.db'
-db = SQLAlchemy(app)
-
-class Post(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  title = db.Column(db.String(300), nullable=False)
-  text = db.Column(db.Text, nullable=False)
+app.config['MAIL_SERVER'] = 'smtp.yourmailserver.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USERNAME'] = 'Павел'
+app.config['MAIL_PASSWORD'] = '30eaapwbjkl'
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
 
 
-@app.route('/index')
+def send_email(subject, body):
+    recipients = Email.query.all()  
+    
+    for recipient in recipients:
+        msg = Message(subject, recipients=[recipient.email])
+        msg.body = 'This is a test email.'
+        mail.send(msg)
+
+
 @app.route('/')
 def index():
   return render_template('index.html')
@@ -23,9 +32,42 @@ def our_projects():
   return render_template('our_projects.html', posts=posts)
 
 
+
 @app.route('/services')
 def services():
   return render_template('services.html')
+
+
+@app.route('/forms')
+def forms():
+  return render_template('forms.html')
+
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    name = request.form.get('name')
+    
+    image = request.files['image']
+    image_data = image.read()
+    
+    new_image = Post(name=name, image=image_data)
+    
+    db.session.add(new_image)
+    db.session.commit()
+    
+    return 'Image uploaded successfully'
+
+
+@app.route('/add_email', methods=['POST'])
+def add_email():
+    email = request.form['email']
+    
+    new_email = Email(email=email)
+    db.session.add(new_email)
+    db.session.commit()
+    
+    return render_template('index.html')
+  
 
 
 if __name__ == "__main__":
---
+++ db.py
@@ -0,0 +1,20 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+from flask_mail import Mail
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask.db'
+db = SQLAlchemy(app)
+
+mail = Mail(app)
+
+class Email(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    
+    
+class Post(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(100))
+  image = db.Column(db.LargeBinary)
